9446e2e2fbab234788fcbc36e1ef634d1a8d9112
Gustavo <gp67009@gmail.com>
2024-08-29 14:49:06 -0300

    Serviços Mobile

--- src/pages/NossosServicos.jsx
+++ src/pages/NossosServicos.jsx
@@ -3,7 +3,7 @@ import '../assets/css/nossosServicos.css'
 
 export function NossosServicos() {
 
-    const scrollToTop= () => {
+    const scrollToTop = () => {
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
@@ -62,6 +62,50 @@ export function NossosServicos() {
                 </div>
 
                 <div className='ns1'>
+
+                    <div className='card3'>
+                        <Link to="/servico4" onClick={scrollToTop}><img className='img-card' src="/img/equipamentos1.svg" alt="" />
+                            <p>Equipamentos Agrícolas</p>
+                        </Link>
+                    </div>
+
+                    <div className='card3'>
+                        <Link to="/servico5" onClick={scrollToTop}><img className='img-card' src="/img/industria1.svg" alt="" />
+                            <p>Serviços de Caldeiraria e Solda na Indústria </p>
+                        </Link>
+                    </div>
+
+                    <div className='card3'>
+                        <Link to="/servico6" onClick={scrollToTop}><img className='img-card' src="/img/alocacao1.svg" alt="" />
+                            <p>Alocação de Mão de Obra Especializada</p>
+                        </Link>
+                    </div>
+
+                </div>
+
+            </section>
+
+            <section className='terceira-section-servicos-mobile'>
+
+                <div className='ns-mobile'>
+
+                    <div className='card3'>
+                        <Link to="/servico" onClick={scrollToTop}><img className='img-card' src="/img/caldeiraria2.svg" alt="" />
+                            <p>Serviços de Caldeiraria Leve, Médio e Grande Porte </p>
+                        </Link>
+                    </div>
+
+                    <div className='card3'>
+                        <Link to="/servico2" onClick={scrollToTop}><img className='img-card' src="/img/tubulacao1.svg" alt="" />
+                            <p>Tanques e Tubulação de Diversas Finalidades</p>
+                        </Link>
+                    </div>
+
+                    <div className='card3'>
+                        <Link to="/servico3" onClick={scrollToTop}><img className='img-card' src="/img/estrutura1.svg" alt="" />
+                            <p>Estruturas Metalicas em Geral</p>
+                        </Link>
+                    </div>
 
                     <div className='card3'>
                         <Link to="/servico4" onClick={scrollToTop}><img className='img-card' src="/img/equipamentos1.svg" alt="" />
